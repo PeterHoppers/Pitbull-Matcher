1687a5c0d5876d8e83e1b6aac92754ff2d5124cf
Peter Hoppe <peterhoppe341@gmail.com>
2024-04-03 13:59:41 -0500

    Added Basic Tracks

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,23 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import AudioPlayer from 'react-h5-audio-player';
+import 'react-h5-audio-player/lib/styles.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [selectedId, setSelectedId] = useState(0);
+
+  const folderName = "BackInTime";
+  const trackName = "backInTimeTTS";
 
   return (
     <>
+      <AudioPlayer
+        autoPlay
+        src={`/Pitbull-Matcher/audio/${folderName}/${trackName}.mp3`}
+        onPlay={e => console.log("onPlay")}
+        // other props here
+      />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,18 +27,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
     </>
   )
 }
